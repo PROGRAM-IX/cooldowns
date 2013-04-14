e5cd912710e7135bbdfb9929f482257d189814ae
James Heslin <program.ix.j@gmail.com>
2013-04-14 03:19:57 +0100

    Fixed an indent error with the licence >.<

--- cooldowns.py
+++ cooldowns.py
@@ -1,20 +1,20 @@
-    """
-    Cooldowns-based game of reactions in Python using Pygame.
-    Copyright (C) 2012 James Heslin (PROGRAM_IX)
+"""
+Cooldowns-based game of reactions in Python using Pygame.
+Copyright (C) 2012 James Heslin (PROGRAM_IX)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     """
 import random, sys, time, pygame
 from pygame.locals import *
